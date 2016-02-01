5cdc293ce7a3274f0e95b1a3000e726f458dfae7
steefdam <steefdam@gmail.com>
2016-02-01 09:59:10 +0100

    Added functionality for stratified sampling

--- R/defSamples.R
+++ R/defSamples.R
@@ -1,31 +1,52 @@
 source("R/randomsamp.R")
+source("R/stratsamp.R")
 
 library(foreach)
 
-defSamples <- function(n, object, samplemethod = "random", q = 0) {
+defSamples <- function(n, object, samplemethod = "random", p = 0) {
   if (samplemethod == "random") {
     # this is the tricky part
     if (class(object) == "SpatialGridDataFrame") {
-      x <- foreach(a = object[[1]], b = sqrt(object[[2]]), .combine = rbind) %do% {
+      samples <- foreach(a = object[[1]], b = sqrt(object[[2]]), .combine = rbind) %do% {
         randomsamp(a, b, n)
       }
-    } 
+    }
     
     if (class(object) == "nummarnonspatial") {
       if (object@dist == "norm") {
-        x <- rnorm(n, object@par[1], object@par[2])
+        samples <- rnorm(n, object@par[1], object@par[2])
       }
       
       if (object@dist == "log") {
-        x <- rlnorm(n, object@par[1], object@par[2])
+        samples <- rlnorm(n, object@par[1], object@par[2])
       }
       
       if (object@dist == "beta") {
-        x <- rbeta(n, object@par[1], object@par[2])
+        samples <- rbeta(n, object@par[1], object@par[2])
       }
     }
   }
-  return(x)
+  
+  if (samplemethod == "strat") {
+    if (class(object) == "SpatialGridDataFrame") {
+      x <- foreach(a = object[[1]], b = sqrt(object[[2]]), .combine = rbind) %do% {
+        as.numeric(test_stratsamp(a, b, n/n, p))
+      }
+
+      samples <- matrix(NA, nrow=length(object[[1]]), ncol=n)
+      
+      for (i in 1:length(object[[1]])) {
+        samples[i, ] <- x[i][[1]]
+      }
+    }
+    
+    if (class(object) == "nummarnonspatial") {
+      if (object$dist == "norm") {
+        samples <- stratsamp(object$par[1], object$par[2], n, p)
+      }
+    }
+  }
+  return(samples)
 }
 
 # mySamplesNorm <- defSamples(n = 10, object = mynorm)
